3e019692d64623117f5201c56aa2fa41b448eb53
Semachew Fasika <eth.saf.1992@gmail.com>
2022-06-27 23:24:43 +0300

    7-edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Holberton School"
+first_3_str = str[0:3]
+last_2_str = str[-2:]
+middle_str = str[1:-1]
+print(f"First 3 letters: {first_3_str}")
+print(f"Last 2 letters: {last_2_str}")
+print(f"Middle str: {middle_str}")
