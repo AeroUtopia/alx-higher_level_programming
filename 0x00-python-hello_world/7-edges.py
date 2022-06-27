9b02cc5f308c0bdaac4560bb86c65aedaa3ea1ad
Semachew Fasika <eth.saf.1992@gmail.com>
2022-06-27 23:28:28 +0300

    7-edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-str = "Holberton"
-first_3_str = str[0:3]
-last_2_str = str[-2:]
-middle_str = str[1:-1]
-print(f"First 3 letters: {first_3_str}")
-print(f"Last 2 letters: {last_2_str}")
-print(f"Middle str: {middle_str}")
+word = "Holberton"
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
