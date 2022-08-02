6341b192efc397f18148b4fb4d1252f56762d370
Semachew Fasika <eth.saf.1992@gmail.com>
2022-08-02 13:41:26 +0300

    12-pascal_triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""
-14-main - Pascals Triangle
+"""Pascal Triangle function
 """
 
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    my_list = [[1]]
-    for count in range(1, n):
-        row = [1]
-        for elem in range(1, count):
-            row.append(my_list[count - 1][elem - 1] + my_list[count - 1][elem])
-        row.append(1)
-        my_list.append(row)
-    return my_list
+    """Pascals Triangle
+    """
+    new = []
+    for i in range(n):
+        add = [1]
+        if i > 0:
+            for j in range(len(new[i - 1])):
+                if j == len(new[i - 1]) - 1:
+                    add.append(1)
+                else:
+                    add.append(new[i - 1][j] + new[i - 1][j + 1])
+        new.append(add)
+    return new
