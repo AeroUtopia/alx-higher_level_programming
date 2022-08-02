73d38795edf5d6bd5c652939a81e820ef281f728
Semachew Fasika <eth.saf.1992@gmail.com>
2022-08-02 13:29:45 +0300

    2-append_write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""Python Function to append string to a file"""
+"""Appends text to a file
+"""
 
 
 def append_write(filename="", text=""):
-    with open(filename, 'a', encoding='utf-8') as f:
-        return f.write(text)
+    """Appends text to filename
+    Args:
+        filename (str): filename/filepath
+        text (str): text to append
+    """
+    if type(filename) is str and type(text) is str:
+        with open(filename, 'a') as fyle:
+            count = fyle.write(text)
+    return count
