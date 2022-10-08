ef0cfc5be2de2698dbe1061517ba43d76e845bd8
Semachew Fasika <eth.saf.1992@gmail.com>
2022-10-08 19:57:37 +0300

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""
-displays all values in the states table of
- hbtn_0e_0_usa where name matches the argument
-"""
+"""takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+where name matches the argument
+and is safe from SQL injections"""
 
-import sys
-import MySQLdb
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states""")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""
-Defines a state model that contain the class definition
- of a State and an instance Base = declarative_base()
-"""
-from lib2to3.pytree import Base
-from sre_parse import State
-from unicodedata import name
-from sqlalchemy import Column, Integer, String
+"""Definition of the State class"""
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    inherits from Base Tips
-    links to the MySQL table states
-    class attribute id that represents a column
-     of an auto-generated, unique integer, can't
-      be null and is a primary key
-    class attribute name that represents a column
-     of a string with maximum 128 characters and
-      can't be null
+    """Class State"""
 
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
