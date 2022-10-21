efe7212d2f88c7cb1d285c776855eef408b6d4b7
Semachew Fasika <eth.saf.1992@gmail.com>
2022-10-21 16:04:49 +0300

    0-hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request as req
-
-if __name__ == "__main__":
-    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        data = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data.decode("utf-8")))
+"""fetches https://intranet.hbtn.io/status using the urllib package
+"""
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    data = response.read()
+    print("Body response:")
+    print("	- type:", type(data))
+    print("	- content:", data)
+    print("	- utf8 content:", data.decode('utf-8'))
